7359b25fc17a278d50b1d52bca956ab41fae7ff6
TejasPote <tejaspote915@gmail.com>
2025-03-26 10:29:53 -0600

    updated train_script

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -2,4 +2,14 @@
 
 cd .. || exit
 
-python train.py 
+python train.py \
+  --train_dir "garbage_data/Train" \
+  --val_dir "garbage_data/Val" \
+  --test_dir "garbage_data/Test" \
+  --epochs 10 \
+  --batch_size 32 \
+  --learning_rate 2e-5 \
+  --weight_decay 1e-4 \
+  --max_len 32 \
+  --num_workers 4 \
+  --checkpoint_dir "checkpoint"
